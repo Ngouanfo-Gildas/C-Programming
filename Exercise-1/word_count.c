31596d8812e2d247ad7de3fb27b87a99ea6ba069
GILDAS NGOUANFO MANFO <gildas.manfo@acesy.net>
2024-08-12 23:40:08 +0100

    Ajout des fichiers de base

---
+++ Exercise-1/word_count.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int is_letter_or_apostrophe(char c) {
+    return isalpha(c) || c == '\'';
+}
+
+int is_sentence_end(char c) {
+    return c == '.' || c == '!' || c == '?';
+}
+
+int main(int argc, char *argv[]){
+
+    char c;
+    int word_count = 0;
+    int sentence_count = 0;
+    int total_words = 0;
+    float average_word_per_sentences = 0.0;
+
+    if (argc != 2) {
+        fprintf(stderr, "\nUse the following syntax to run the progam: %s <filename>\n\n", argv[0]);
+        exit(1);
+    }
+
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL) {
+        fprintf(stderr, "\nError opening file %s.\n\n",argv[1]);
+        exit(1);
+    }
+
+    printf("\nThis program counts the words and sentences in file %s.\n\n", argv[1]);
+
+// isspace(c) est vrai si c est un blanc
+
+    while ((c = fgetc(file)) != EOF ){
+        if (is_sentence_end(c))
+        {
+            total_words += word_count;
+            sentence_count++;
+            printf("Sentence %d : %d words.\n",sentence_count,word_count);
+            word_count = 0;
+        }
+        
+        if(isspace(c)){
+            word_count++;
+        }
+        
+    }
+
+    average_word_per_sentences = (float)total_words/(float)sentence_count;
+
+    printf("\nFile \"%s\" contains %d words in %d sentences for an average of %.2f words per sentence.\n\n", argv[1], total_words, sentence_count, average_word_per_sentences);
+
+    fclose(file);
+
+    return 0;
+}
+
+// https://github.com/MeliArchilleGael/quiz-app.git
+
+// page 85
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # C-Programming
+
+## Exercise 1
+
+Write a C program that takes the name of a file as a command-line argument, opens the file, reads through it to determine the number of words in each sentence, displays the total number of words and sentences, and computes the average number of words per sentence. The results should be printed in a table (as standard output), such as shown below:
+
+This program counts the words and sentences in file "comp.text".
+Sentence: 1 Words: 29
+Sentence: 2 Words: 41
+Sentence: 3 Words: 16
+Sentence: 4 Words: 22
+Sentence: 5 Words: 44
+Sentence: 6 Words: 14
+Sentence: 7 Words: 32
+File "comp.text" contains 198 words words in  7 sentences for an average of 28.3 words per sentence.
+
+In this program, you should count a word as any contiguous sequence of letters, and apostrophes should be ignored. Thus, "O'Henry", "government's", and "friend's" should each be considered as one word.
